1fd22747b8b8663df8379d1fdbba529e5fff4180
Alaeddine GARRAOUI <76453888+AlaGrr@users.noreply.github.com>
2022-05-05 16:30:04 +0200

    App | add subtitle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import ShoppingList from "./ShoppingList";
+import Connection from "./Connection";
 import Home from "./Home";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
@@ -26,6 +27,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shopping-list" element={<ShoppingList />} />
+          <Route path="/Connection" element={<Connection />} />
         </Routes>
       </Box>
     </div>
